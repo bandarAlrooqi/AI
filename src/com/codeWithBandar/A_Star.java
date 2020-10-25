122e3b0b17f4029dc47e47aa210e7c2445faea4c
bandarAlrooqi <officialalrooqi@gmail.com>
2020-10-25 20:52:21 +0300

    add comments

--- src/com/codeWithBandar/A_Star.java
+++ src/com/codeWithBandar/A_Star.java
@@ -1,8 +1,5 @@
 package com.codeWithBandar;
 
-/*
-This code was written by Bandar 
- */
 import java.util.*;
 
 public class A_Star {
@@ -12,7 +9,7 @@ public class A_Star {
             this.state = puzzle;
         }
         @Override
-        public String toString() {
+        public String toString() { // displaying the state in understandable way ..
             StringBuilder m = new StringBuilder();
             for (int i = 0; i < state.length(); i++) {
 
@@ -27,17 +24,17 @@ public class A_Star {
         }
 
         @Override
-        public int compareTo(Object o) {
-            return   (A_Star.calculateHeuristicValue(this.state)+g)-(A_Star.calculateHeuristicValue(((Node)o).state)+g);
+        public int compareTo(Object o) { // using compareTo method to make a priority queue ..
+            return   (A_Star.calculateHeuristicValue(this.state)+ actualCost)-(A_Star.calculateHeuristicValue(((Node)o).state)+ actualCost);
         }
     }
-    static String goal;
-    static int g;
-    Map<String,Node> state ;
-    Map<Node, List<Node>> map;
-    Set<String> visited;
-    ArrayList<Node>path;
-    int zg= 0;
+    static String goal ; // the goal state is static to help using it in other classes
+    static int actualCost; // the actual cost used with heuristic value (estimated cost) to help find the shortest path to the goal
+    Map<String,Node> state ; // storing all the nodes
+    Map<Node, List<Node>> map; // storing the node and its children
+    Set<String> visited; // set of visited node to help not visiting a node twice
+    ArrayList<Node>path; // an optional list used to store the whole path from initial state to the goal state .
+    int numberOfNodeGenerated = 0; // calculating the number of node the explored ( this DOES NOT mean that the node is visited nor checked )
     public A_Star(String initial, String goal){
 
         A_Star.goal = goal;
@@ -50,34 +47,38 @@ public class A_Star {
 
         state.putIfAbsent(initial,node);
         map.putIfAbsent(node,new ArrayList<>());
-        search();
+
+        search();//kicking the program to start
 
     }
-    private boolean isGoal(String current){
+    private boolean isGoal(String current){ // this method is used to check if the current node is a goal node
     if(goal.equals(current)){
         System.out.println("GOAL!!");
         System.out.println("Path size = "+path.size());
         System.out.println("Node visited = "+visited.size());
-        System.out.println("Number of Node explored before the goal level = "+zg);
-        System.out.println("G = "+g);
+        System.out.println("Number of Node explored before the goal level = "+ numberOfNodeGenerated);
         return true;
     }
     return false;
     }
-    static int calculateHeuristicValue(String current){
+    static int calculateHeuristicValue(String current){ // this method is calculating the Heuristic value based on Hamming distance ( Number of misplaced numbers )
         int num =0;
         for (int i = 0; i < current.length(); i++) {
+
             if(current.charAt(i)=='0')continue;
-            if(current.charAt(i) != goal.charAt(i))num++;
+
+            if(current.charAt(i) != goal.charAt(i))
+                num++;
         }
 
         return num;
     }
-    static int minhatinDistance(String current){
+    static int manhattanDistance(String current){ // calculating Heuristic value using Manhattan distance (how far is the number from its goal position )
         int sum=0;
-        char[][]goal=new char[3][];
+        char[][]goal=new char[3][]; // converting goal state into 2-D array to help detecting the row and column
         char[][]currentState = new char[3][];
-        goal[0]=A_Star.goal.substring(0,3).toCharArray();
+
+        goal[0]=A_Star.goal.substring(0,3).toCharArray(); // getting the rows ..
         goal[1]=A_Star.goal.substring(3,6).toCharArray();
         goal[2]=A_Star.goal.substring(6,9).toCharArray();
 
@@ -87,12 +88,18 @@ public class A_Star {
 
         for (int i = 0; i < goal.length; i++) {
             for (int j = 0; j <goal[i].length ; j++) {
-                if(currentState[i][j]==goal[i][j] || currentState[i][j]=='0')continue;
+                if(currentState[i][j]==goal[i][j] || currentState[i][j]=='0')continue; // if the number in its goal position or if the number is space (0) then continue
 
-                var value = indexOf(goal,currentState[i][j]);
-                int row =Integer.parseInt(value.charAt(0)+"");
+                var value = indexOf(goal,currentState[i][j]); // get the row and column to help calculating how far is the number from its position
+                int row = Integer.parseInt(value.charAt(0)+"");
                 int col = Integer.parseInt(value.charAt(1)+"");
 
+                /*
+                once we get here that means we only have 3-cases :
+                1- the number is in the same row as its goal position
+                2- the number is in the same column as its goal position
+                3- the number is neither in the row or column as its goal position
+                 */
                 if(row == i) sum+=Math.abs((j+1)-(col+1));
                 else if(col == j) sum+=Math.abs((i+1)-(row+1));
                 else sum+=((Math.abs((j+1)-(col+1))) + (Math.abs((i+1)-(row+1))));
@@ -100,7 +107,7 @@ public class A_Star {
         }
         return sum;
     }
-    private static String indexOf(char[][] array, char value){
+    private static String indexOf(char[][] array, char value){ // a helper method to find the row and column for manhattanDistance method above
         for (int i = 0; i <array.length ; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 if(array[i][j]==value)return i+""+j;
@@ -108,17 +115,17 @@ public class A_Star {
         }
         return null;
     }
-    private String generate(String position,String currentState){
+    private String generate(String position,String currentState){ // generate a node for the current node
         StringBuilder m = new StringBuilder(currentState);
         for (int  i = 0; i<9 ; i++) {
-            if(currentState.charAt(i) != '0')continue;
-            if(position.equals("up") && i <= 2)return null;
+            if(currentState.charAt(i) != '0')continue; // we are looking for the space that can move , as we cant move numbers
+            if(position.equals("up") && i <= 2)return null; // avoiding edges or moving towards edges
             if(position.equals("left") &&  (i== 0 || i==3||i==6))return null;
             if(position.equals("right") && (i==2||i==5||i==8))return null;
             if(position.equals("down") && i>=6)return null;
 
             switch (position) {
-                case "up" -> {
+                case "up" -> { // switching the numbers ..
                     char temp = currentState.charAt(currentState.indexOf('0') - 3);
                     m.deleteCharAt(i);
                     m.insert(i, temp);
@@ -157,31 +164,33 @@ public class A_Star {
 
     }
     private void addChildren(List<Node>list, Node node){
-        g++;
-        directions(list, generate("up",node.state), generate("down",node.state));
+        actualCost++; // the actual cost it 8- puzzle is considered to be a level of the node .. we increase it here as we add children which means we are adding a new level
+        directions(list, generate("up",node.state), generate("down",node.state)); // sending the 4 nodes to the other method to add it to our graph
         directions(list, generate("left",node.state), generate("right",node.state));
     }
-    private void directions(List<Node> list, String up, String down) {
-        if(up !=null){var root = new Node(up);list.add(root);map.putIfAbsent(root,new ArrayList<>());}
+    private void directions(List<Node> list, String up, String down) { // helper method for the above one
+        if(up !=null){var root = new Node(up);list.add(root);map.putIfAbsent(root,new ArrayList<>());} // if the node is not null ( which means it is not at the edges ) then create it
         if(down!=null){var root = new Node(down);list.add(root);map.putIfAbsent(root,new ArrayList<>());}
     }
-    private void search(){
-        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
-        priorityQueue.add((Node)map.keySet().toArray()[0]);
-        while (!priorityQueue.isEmpty()){
-            var node = priorityQueue.poll();
-            System.out.println("\n     |\n     |\n     |\n    \\ /\n");
-            System.out.println(node+"\tH= "+minhatinDistance(node.state));
-            visited.add(node.state);
-            path.add(node);
-            if(isGoal(node.state))return;
 
-            addChildren(map.get(node),node);
-            for(var i : map.get(node)){
-                if(visited.contains(i.state))
+    private void search(){
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>(); // initializing priority queue of nodes
+        priorityQueue.add((Node)map.keySet().toArray()[0]); // adding our initial state to the queue
+        while (!priorityQueue.isEmpty()){
+            var node = priorityQueue.poll(); // removing the node from the queue to be visited and checked
+            System.out.println("\n     |\n     |\n     |\n    \\ /\n"); // decoration for the console print
+            System.out.println(node+"\tH= "+ manhattanDistance(node.state)); // printing the heuristic value next to the node
+            visited.add(node.state); // adding the node to visited listed in order to not visit it again
+            path.add(node); // adding the node to our path
+            if(isGoal(node.state))return; // checking if the current node is the goal
+
+            addChildren(map.get(node),node); // if it is not the goal then generate children
+
+            for(var i : map.get(node)){ // adding the generated nodes to our queue
+                if(visited.contains(i.state)) // if the node has been visited before then just ignore the process and continue
                     continue;
                 priorityQueue.add(i);
-                zg++;
+                numberOfNodeGenerated++;
             }
         }
     }
--- src/com/codeWithBandar/Main.java
+++ src/com/codeWithBandar/Main.java
@@ -1,65 +1,14 @@
 package com.codeWithBandar;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-//        EightPuzzle puzzle =new EightPuzzle(new int[][]{
-//                {1,4,3},
-//                {7,0,6},
-//                {5,8,2}
-//        });
-//       var puzzleUsingString = new EightPuzzleUsingString("724506831","123456780");
-//        var ids = new IDS();
-//        for(char i ='A';i<='Q';i++)
-//            ids.addNode(i+"");
-//        ids.addEdge("A","B");
-//        ids.addEdge("A","C");
-//        ids.addEdge("B","D");
-//        ids.addEdge("B","E");
-//        ids.addEdge("C","F");
-//        ids.addEdge("C","G");
-//        ids.addEdge("D","H");
-//        ids.addEdge("D","I");
-//        ids.addEdge("E","J");
-//        ids.addEdge("E","k");
-//        ids.addEdge("J","P");
-//        ids.addEdge("J","Q");
-//        ids.addEdge("F","L");
-//        ids.addEdge("F","M");
-//        ids.addEdge("G","N");
-//        ids.addEdge("G","O");
-//        ids.search("A","O",2);
-//DLL dll = new DLL();
-//dll.add(1);
-//dll.add(2);
-//dll.add(3);
-//dll.delete(1);
-//dll.print();
-
-
-
+        EightPuzzle puzzle =new EightPuzzle(new int[][]{
+                {1,4,3},
+                {7,0,6},
+                {5,8,2}
+        });
+       var puzzleUsingString = new EightPuzzleUsingString("724506831","123456780");
        A_Star a_star = new A_Star("283164705","123804765");
-        //var puzzleUsingString = new EightPuzzleUsingString("283164705","123804765");
-//        String goal ="";
-//        goal=fill(goal,'w');
-//        goal=fill(goal,'r');
-//        goal=fill(goal,'g');
-//        goal=fill(goal,'y');
-//        goal=fill(goal,'b');
-//        goal= fill(goal,'o');
-//        System.out.println(goal);
-//        var i = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-//        System.out.println(i.format(LocalDateTime.now()));
-//        Period period = Period.between(LocalDate.of(1995,4,26),LocalDate.now());
-//        System.out.println("Years\t\tMonths\t\tDays");
-//        System.out.println(period.getYears() +"\t\t\t"+period.getMonths()+"\t\t\t"+period.getDays());
-    }
-    public static String fill(String goal,char c){
-        for (int i = 0; i < 9; i++) {
-            goal+=c;
-        }
-        return goal;
     }
 }
